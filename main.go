eab61aee03248624380df50cd1ba6edc79d114ab
yash621 <yg17381@gmail.com>
2022-01-17 00:16:52 +0530

    buttons setup

--- main.go
+++ main.go
@@ -2,35 +2,44 @@ package main
 
 import (
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
 )
 
 func main() {
 	a := app.New()
 	w := a.NewWindow("Hello World")
-	historyBtn := widget.NewButton("history", func() {
+	historyBtn := widget.NewButton("History", func() {
 		w.SetContent(widget.NewLabel("history"))
 	})
-	backBtn := widget.NewButton("back", func() {})
-	clearBtn := widget.NewButton("clear", func() {})
-	openBtn := widget.NewButton("open", func() {})
-	closeBtn := widget.NewButton("close", func() {})
-	divideBtn := widget.NewButton("divide", func() {})
+	backBtn := widget.NewButton("Back", func() {})
+	clearBtn := widget.NewButton("Clear", func() {})
+	openBtn := widget.NewButton("(", func() {})
+	closeBtn := widget.NewButton(")", func() {})
+	divideBtn := widget.NewButton("/", func() {})
 	sevenBtn := widget.NewButton("7", func() {})
 	eightBtn := widget.NewButton("8", func() {})
 	nineBtn := widget.NewButton("9", func() {})
-	multiplyBtn := widget.NewButton("multiply", func() {})
+	multiplyBtn := widget.NewButton("*", func() {})
 	fourBtn := widget.NewButton("4", func() {})
 	fiveBtn := widget.NewButton("5", func() {})
 	sixBtn := widget.NewButton("6", func() {})
-	subtractBtn := widget.NewButton("subtract", func() {})
+	subtractBtn := widget.NewButton("-", func() {})
 	oneBtn := widget.NewButton("1", func() {})
 	twoBtn := widget.NewButton("2", func() {})
 	threeBtn := widget.NewButton("3", func() {})
-	addBtn := widget.NewButton("add", func() {})
+	addBtn := widget.NewButton("+", func() {})
 	zeroBtn := widget.NewButton("0", func() {})
 	decimalBtn := widget.NewButton(".", func() {})
 	equalsBtn := widget.NewButton("=", func() {})
-	w.SetContent(widget.NewLabel("Hello World!"))
+	hello := widget.NewLabel("Hello")
+	w.SetContent(container.NewVBox(hello, container.NewGridWithColumns(1,
+		container.NewGridWithColumns(2, historyBtn, backBtn),
+		container.NewGridWithColumns(4, clearBtn, openBtn, closeBtn, divideBtn),
+		container.NewGridWithColumns(4, nineBtn, eightBtn, sevenBtn, multiplyBtn),
+		container.NewGridWithColumns(4, fourBtn, fiveBtn, sixBtn, subtractBtn),
+		container.NewGridWithColumns(4, twoBtn, oneBtn, threeBtn, addBtn),
+		container.NewGridWithColumns(2, container.NewGridWithColumns(2, zeroBtn, decimalBtn), equalsBtn),
+	)))
 	w.ShowAndRun()
 }
